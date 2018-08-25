a2d1d6ca87fafb289b468f89082de62182a86222
Seiei Miyagi <hanachin@gmail.com>
2018-08-25 14:06:53 +0900

    update

--- test.sh
+++ test.sh
@@ -3,7 +3,10 @@
 set -ex
 
 if [ ! -e mruby ]; then
-    curl http://forum.mruby.org/download/source/mruby-1.2.0.tar.gz | tar zxf -
+    curl -L https://github.com/mruby/mruby/archive/1.4.1.zip -o mruby.zip
+    unzip mruby.zip
+    mv mruby-1.4.1 mruby
+    rm mruby.zip
 fi
 cp build_config.rb mruby/build_config.rb
 (cd mruby && make test)
